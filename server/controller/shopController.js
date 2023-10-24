0099ec21094bcad261358122979b5beedc840f2a
RifnaC <rifnazar2@gmail.com>
2023-10-24 19:57:13 +0530

    update shop image

--- server/controller/shopController.js
+++ server/controller/shopController.js
@@ -3,44 +3,23 @@ const { log } = require('handlebars');
 const Shopdb = require('../models/shopModel');
 const cloudinary = require('../services/cloudinary');
 const path = require('path')
-// const upload = require('../middlewares/multer');
 const multer = require('multer');
 
+// accessing multer middleware storage
 const storage = multer.diskStorage({
-  destination: function(req, file, cb){
+  destination: function (req, file, cb) {
     cb(null, 'uploads')
   },
-  filename:function(req, file, cb){
+  filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now() + file.originalname.split('.').pop())
   },
 });
 
 const upload = multer({ storage: storage });
 
-// const cloudinary = require('cloudinary').v2;
-// create and save new shop
-// exports.create = async (req, res) => {
-//     if(!req.body){
-//       res.status(400).send({message: 'Content can not be empty'})
-//       return;
-//     }
-//     const { name, openingTime, closingTime, shopImg, address} = req.body;
-
-//     const shop = new Shopdb({
-//     name,
-//     openingTime,
-//     closingTime,
-//     shopImg,
-//     address,
-//   });
-//   const savedShop = await shop.save(); 
-//   console.log(savedShop);
-//   res.redirect('/shop');
-// };
-
-
-exports.create =  async (req, res) => {
-  upload.single('shopImg')(req, res,async(err)=> {
+// create new shop
+exports.create = async (req, res) => {
+  upload.single('shopImg')(req, res, async (err) => {
     if (err) {
       res.status(500).send({ message: err.message });
       return;
@@ -52,27 +31,26 @@ exports.create =  async (req, res) => {
     const { name, openingTime, closingTime, address } = req.body;
     const shopImg = req.file.path;
 
-    cloudinary.uploader.upload(shopImg,  (cloudinaryErr, result) => {
+    cloudinary.uploader.upload(shopImg, (cloudinaryErr, result) => {
       if (cloudinaryErr) {
         res.status(500).send({ message: cloudinaryErr.message });
         return;
       }
-      console.log(result);
-
+      // console.log(result);
       const shop = new Shopdb({
         name,
         openingTime,
         closingTime,
-        shopImg: result.secure_url ,
+        shopImg: result.secure_url,
         address,
       });
       shop.save()
       .then(savedShop => {
-        console.log(savedShop);
+        // console.log(savedShop);
         res.redirect('/shop');
-      })
+       })
       .catch(saveErr => {
-        console.log('hi')
+        // console.log('hi')
         res.status(500).send({ message: saveErr.message });
       });
     });
@@ -85,35 +63,35 @@ exports.find = (req, res) => {
   if (req.query.id) {
     const id = req.query.id;
     Shopdb.findById(id)
-      .then(data => {
-        if (!data) {
-          res.status(404).send({ message: "Not found shop with id" + id })
-        } else {
-          res.send(data)
-        }
-      })
-      .catch(err => {
-        res.status(500).send({ message: "Error in retrieving shop with id" + id })
-      })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({ message: "Not found shop with id" + id })
+      } else {
+        res.send(data)
+      }
+    })
+    .catch(err => {
+      res.status(500).send({ message: "Error in retrieving shop with id" + id })
+    })
   } else {
     Shopdb.find()
-      .then(shop => {
-        // If it's an API request, send JSON data
-        if (req.path === '/api/shops') {
-          res.json(shop);
-        } else {
-          // If it's a web request, render the "shop" page with the data
-          res.render('shop', { shops: shop });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({ message: err.message || "Some error occurred while retrieving shop information" });
-      });
+    .then(shop => {
+      // If it's an API request, send JSON data
+      if (req.path === '/api/shops') {
+        res.json(shop);
+      } else {
+        // If it's a web request, render the "shop" page with the data
+        res.render('shop', { shops: shop });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({ message: err.message || "Some error occurred while retrieving shop information" });
+    });
   }
 }
 
 
-// Update a new identified admin by  admin id
+// Update a new identified shop by  shop id
 exports.update = (req, res) => {
   if (!req.body) {
     return res.status(400).send({ message: "Data to update can not be empty" })
--- server/services/cloudinary.js
+++ server/services/cloudinary.js
@@ -1,6 +1,5 @@
 const cloudinary = require('cloudinary').v2;
 
-// cloudinary requests
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME, 
     api_key: process.env.API_KEY, 
