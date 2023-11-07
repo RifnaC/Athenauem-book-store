feab2639cb44fa3f9cea7be9a1383b788f183daa
Rifna <102509964+RifnaC@users.noreply.github.com>
2023-11-07 10:10:54 +0530

    update featured category design with icon

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,34 +1,97 @@
-(function ($) {
-    $(function () {
+/**
+ * @description Change Home page slider's arrows active status
+ */
+function updateSliderArrowsStatus(
+    cardsContainer,
+    containerWidth,
+    cardCount,
+    cardWidth
+  ) {
+    if (
+      $(cardsContainer).scrollLeft() + containerWidth <
+      cardCount * cardWidth + 15
+    ) {
+      $("#slide-right-container").addClass("active");
+    } else {
+      $("#slide-right-container").removeClass("active");
+    }
+    if ($(cardsContainer).scrollLeft() > 0) {
+      $("#slide-left-container").addClass("active");
+    } else {
+      $("#slide-left-container").removeClass("active");
+    }
+  }
   
-      $('.js-carousel').slick({
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        prevArrow: '.js-ag-carousel-arrow_prev',
-        nextArrow: '.js-ag-carousel-arrow_next',
+  $(document).ready(function() {
+    // Scroll products' slider left/right
+    let div = $("#cards-container");
+    let cardCount = $(div)
+      .find(".cards")
+      .children(".card").length;
+    let speed = 1000;
+    let containerWidth = $(".container").width();
+    let cardWidth = 250;
   
-        variableWidth: true,
-        centerMode: true,
-        responsive: [
+    updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
+  
+    // Remove scrollbars
+    $("#slide-right-container").click(function(e) {
+      if ($(div).scrollLeft() + containerWidth < cardCount * cardWidth) {
+        $(div).animate(
           {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 2,
-              variableWidth: false,
-              centerMode: false
-            }
+            scrollLeft: $(div).scrollLeft() + cardWidth
           },
           {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              variableWidth: false,
-              centerMode: false
+            duration: speed,
+            complete: function() {
+              setTimeout(function() {
+                updateSliderArrowsStatus(
+                  div,
+                  containerWidth,
+                  cardCount,
+                  cardWidth
+                );
+              }, 1005);
             }
           }
-        ]
-      });
-  
+        );
+      }
+      updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
     });
-  })(jQuery);
+    $("#slide-left-container").click(function(e) {
+      if ($(div).scrollLeft() + containerWidth > containerWidth) {
+        $(div).animate(
+          {
+            scrollLeft: "-=" + cardWidth
+          },
+          {
+            duration: speed,
+            complete: function() {
+              setTimeout(function() {
+                updateSliderArrowsStatus(
+                  div,
+                  containerWidth,
+                  cardCount,
+                  cardWidth
+                );
+              }, 1005);
+            }
+          }
+        );
+      }
+      updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
+    });
+  
+    // If resize action occurred then update the container width value
+    $(window).resize(function() {
+      try {
+        containerWidth = $("#cards-container").width();
+        updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
+      } catch (error) {
+        console.log(
+          `Error occurred while trying to get updated slider container width: ${error}`
+        );
+      }
+    });
+  });
+  
