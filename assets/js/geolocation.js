de18440bf25e7680d512e1aaa9f4f9e27a1d4fae
Rifna <102509964+RifnaC@users.noreply.github.com>
2023-11-24 20:55:40 +0530

    update address field by geolocation

--- assets/js/geolocation.js
+++ assets/js/geolocation.js
@@ -1,27 +1,84 @@
-// 
-    
-// // function showPosition(position) {
-// //     const latitude = position.coords.latitude;
-// //     const longitude = position.coords.longitude;
-
-// //     // For example, you could update the value of the input field with the obtained coordinates:
-// //     document.getElementById("address").value = "Latitude: " + latitude + ", Longitude: " + longitude;
-// // }
-
-
 // const x = document.getElementById("address");
 
 // function getLocation() {
-//     if (navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(showPosition, handleLocationError);
-//     } else { 
-//         swal.fire({
-//             title: "Error", 
-//             text: "Geolocation is not supported by this browser.", 
-//             confirmButtonColor: "#15877C"
-//         });
-//     }
+//   if (navigator.geolocation) {
+//     navigator.geolocation.watchPosition(showPosition);
+//   } else { 
+//     swal.fire({
+//         title: "Geolocation is not supported by this browser.",
+//         confirmButtonColor: "#3085d6",
+//     })
+//   }
 // }
+    
+// function showPosition(position) {
+//     const latitude = position.coords.latitude;
+//     const longitude = position.coords.longitude;
+
+//     const geocoder = new google.maps.Geocoder();
+//     geocoder.geocode({
+//       latitude,
+//       longitude,
+//     }, (results, status) => {
+//       if (status === 'OK') {
+//         // The results array contains an array of addresses
+//         const address = results[0];
+//         console.log(address);
+//       } else {
+//         // An error occurred
+//         console.error(status);
+//       }
+//     });
+//     // For example, you could update the value of the input field with the obtained coordinates:
+//     document.getElementById("address").value = "Latitude: " + address ;
+// }
+
+
+
+
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8,
+    center: { lat: 40.731, lng: -73.997 },
+  });
+  const geocoder = new google.maps.Geocoder();
+  const infowindow = new google.maps.InfoWindow();
+
+  document.getElementById("submit").addEventListener("click", () => {
+    geocodeLatLng(geocoder, map, infowindow);
+  });
+}
+
+function geocodeLatLng(geocoder, map, infowindow) {
+  const input = document.getElementById("address").value;
+  const latlngStr = input.split(",", 2);
+  const latlng = {
+    lat: parseFloat(latlngStr[0]),
+    lng: parseFloat(latlngStr[1]),
+  };
+
+  geocoder
+    .geocode({ location: latlng })
+    .then((response) => {
+      if (response.results[0]) {
+        map.setZoom(11);
+
+        const marker = new google.maps.Marker({
+          position: latlng,
+          map: map,
+        });
+
+        infowindow.setContent(response.results[0].formatted_address);
+        infowindow.open(map, marker);
+      } else {
+        window.alert("No results found");
+      }
+    })
+    .catch((e) => window.alert("Geocoder failed due to: " + e));
+}
+
+window.initMap = initMap;
 
 // function showPosition(position) {
 //     const latitude = position.coords.latitude;
@@ -29,7 +86,6 @@
 
 //     // Make a reverse geocoding request to get the place name
 //     const geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyAFROiwMjkH3tIvHePQEwlzOkQ3bUkPZ7E`;
-
 //     fetch(geocodingUrl)
 //         .then(response => response.json())
 //         .then(data => {
@@ -89,62 +145,21 @@
 // }
 
 
-
-
 // const x = document.getElementById("address");
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(async(showPosition) =>{
+//     const latitude = position.coords.latitude;
+//     const longitude = position.coords.longitude;
 
-// // function getLocation() {
-// //   if (navigator.geolocation) {
-// //     navigator.geolocation.watchPosition(showPosition);
-// //   } else { 
-// //     swal.fire({
-// //         title: "Geolocation is not supported by this browser.",
-// //         confirmButtonColor: "#3085d6",
-// //     })
-// //   }
-// // }
-function getLocation() {
-  if (navigator.geolocation) {
-        navigator.geolocation.watchPosition(showPosition);
-      } else { 
-        swal.fire({
-            title: "Geolocation is not supported by this browser.",
-            confirmButtonColor: "#3085d6",
-        })
-      }
-}
+//     const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyAFROiwMjkH3tIvHePQEwlzOkQ3bUkPZ7E`);
 
-function showPosition(position) {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
-  
-      // For example, you could update the value of the input field with the obtained coordinates:
-      document.getElementById("address").value = "Latitude: " + latitude + ", Longitude: " + longitude;
-  }
-// This function is called when the user clicks the UI button requesting
-// a geocode of a place ID.
-function geocodePlaceId(geocoder, map, infowindow) {
-  const placeId = document.getElementById("place-id").value;
+//     const data = await response.json();
+//     console.log(data.address, "address", data);
 
-  geocoder
-    .geocode({ placeId: placeId })
-    .then(({ results }) => {
-      if (results[0]) {
-        map.setZoom(11);
-        map.setCenter(results[0].geometry.location);
+//     x.value = data.address.City + ',' + data.address.Subregion + ',' + data.address.Region;
+//   });
 
-        const marker = new google.maps.Marker({
-          map,
-          position: results[0].geometry.location,
-        });
+// }
 
-        infowindow.setContent(results[0].formatted_address);
-        infowindow.open(map, marker);
-      } else {
-        window.alert("No results found");
-      }
-    })
-    .catch((e) => window.alert("Geocoder failed due to: " + e));
-}
 
-window.getLocation = getLocation;
+
