1c9b431656f64d6224b0730b450f3c43983e0f43
Rifna <102509964+RifnaC@users.noreply.github.com>
2023-12-15 23:52:07 +0530

    update checkout section

--- server/controller/checkoutController.js
+++ server/controller/checkoutController.js
@@ -3,11 +3,10 @@ const Books = require('../models/products');
 const { category } = require('../services/render');
 const path = require('path');
 const mongoose = require('mongoose');
-const userController = require('../controller/userController');
+const Users = require('../models/userModel');
 
 exports.checkout = async(req, res) => {
     const id = req.user.id;
-    const user = await userController.findById(id);
-    const adr = user.addresses[0];
-    res.render('checkout')
+    const user = await Users.findById(id);
+    res.render('checkout', {user: user});
 }
