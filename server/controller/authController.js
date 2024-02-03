3827013d1bbbd0596f46000ed3206229f43ab718
Rifna <102509964+RifnaC@users.noreply.github.com>
2024-02-03 11:39:09 +0530

    update login section

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken');
 const saltRounds = 10;
 const session = require('express-session');
 const JWT_SECRET = process.env.JWT_SECRET;
+const cookie = require('cookie');
 // ***********************user Management********************************
 // Register and save new user
 const signToken = (id,user)=> {
@@ -73,8 +74,8 @@ exports.login = async(req, res) => {
             status: (user || admin).status,
         };
         const token = signToken((user || admin)._id,data);
-        req.session.token = token;
-        // console.log(token);
+        res.cookie('token', token, { httpOnly: true, secure: true });
+        // req.session.token = token;
         if (data.role !== 'User') {
             res.redirect('/dashboard');
         } else {
@@ -93,14 +94,16 @@ exports.login = async(req, res) => {
 // Logout function
 exports.logout = async(req, res) => {
     // Clear the session and redirect to the login page
-    req.session.destroy((err) => {
-        if (err) {
-            console.error('Error destroying session:', err);
-            res.status(500).send({ message: 'Internal Server Error' });
-        } else {
-            res.redirect('/login');
-        }
-    });
+    // req.session.destroy((err) => {
+    //     // if (err) {
+    //     //     console.error('Error destroying session:', err);
+    //     //     res.status(500).send({ message: 'Internal Server Error' });
+    //     // } else {
+    //         res.redirect('/login');
+    //     }
+    // });
+    res.clearCookie('token');
+    res.redirect('/login');
 };
 
 
